2719573b02a8260e932549f9411593b3f1190f8a
Ivan Korobkin <korivval@gmail.com>
2018-01-25 11:39:25 +0000

    Added profiles create action, tweaked flash messages

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,8 +1,28 @@
 class ProfilesController < ApplicationController
     
-    # Get to /users/:user_id/profile/new
+    # GET to /users/:user_id/profile/new
     def new
         # Render blank profile form
         @profile = Profile.new
     end
+    
+    # POST /users/:user_id/profile
+    def create
+        # Make sure we have a user filling out forms
+        @user = User.find(params[:user_id])
+        # Create profile linked to the specific user
+        @profile = @user.build_profile( profile_params )
+        if @profile.save
+            flash[:success] = "Profile updated!"
+            redirect_to root_path
+        else
+            render action: :new
+            flash[:warning] = "Sorry, something went wrong."
+        end
+    end
+    
+    private
+        def profile_params
+            params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_num, :contact_email, :description) 
+        end
 end
